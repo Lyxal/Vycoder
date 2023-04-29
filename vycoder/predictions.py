d01aa320b771ee87260c2a3e45e7cf4f159c1222
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-04-29 13:26:32 +1000

    Update data
    doesn't make much difference

--- vycoder/predictions.py
+++ vycoder/predictions.py
@@ -1,12 +1,16 @@
 import csv
 from . import config
 
-data_path = "vycoder/TrainingData.csv" if config.training else "vycoder/Data.csv"
+data_path = (
+    "vycoder/TrainingData.csv" if config.training else "vycoder/Data.csv"
+)
 
-frequencies = [0]*256
+frequencies = [0] * 256
 
 max_distance = 8
-positions = [[[0]*256 for _ in range(257)] for _ in range(max_distance)]  # dimensions [max_distance, 257, 256]
+positions = [
+    [[0] * 256 for _ in range(257)] for _ in range(max_distance)
+]  # dimensions [max_distance, 257, 256]
 
 with open(data_path, newline="") as f:
     for row in csv.reader(f):
@@ -14,16 +18,15 @@ with open(data_path, newline="") as f:
         for i in range(len(row)):
             frequencies[row[i]] += 1
             for j in range(max_distance):
-                if i-j-1 >= 0:
-                    positions[j][row[i-j-1]][row[i]] += 1
+                if i - j - 1 >= 0:
+                    positions[j][row[i - j - 1]][row[i]] += 1
                 else:
                     positions[j][256][row[i]] += 1
 
 pairs = positions[0]
 
-
 def uniform(x):
-    return [1]*256
+    return [1] * 256
 
 
 def frequency(x):
@@ -31,41 +34,70 @@ def frequency(x):
 
 
 def frequency_plus_uniform(alpha):
-    return lambda x: [x+alpha*sum(frequencies)/256 for x in frequencies]
+    return lambda x: [x + alpha * sum(frequencies) / 256 for x in frequencies]
 
 
 def pair_frequency(alpha, beta):
-    lookup = [[pairs[x][y]*sum(frequencies) + alpha*frequencies[y]*sum(pairs[x]) + sum(frequencies)*sum(pairs[x])*beta for y in range(256)] for x in range(257)]
-    assert all(max(row)/sum(row) < 0.5 for row in lookup)
+    lookup = [
+        [
+            pairs[x][y] * sum(frequencies)
+            + alpha * frequencies[y] * sum(pairs[x])
+            + sum(frequencies) * sum(pairs[x]) * beta
+            for y in range(256)
+        ]
+        for x in range(257)
+    ]
+    assert all(max(row) / sum(row) < 0.5 for row in lookup)
+
     def f(lst):
         if len(lst):
             x = lst[-1]
         else:
             x = 256
         return lookup[x]
+
     return f
 
 
 def pair_frequency2(alpha, beta):
-    lookup = [[pairs[x][y]*(sum(frequencies)+256*alpha) + beta*(frequencies[y] + alpha) for y in range(256)] for x in range(257)]
-    assert all(max(row)/sum(row) < 0.5 for row in lookup)
+    lookup = [
+        [
+            pairs[x][y] * (sum(frequencies) + 256 * alpha)
+            + beta * (frequencies[y] + alpha)
+            for y in range(256)
+        ]
+        for x in range(257)
+    ]
+    assert all(max(row) / sum(row) < 0.5 for row in lookup)
+
     def f(lst):
         if len(lst):
             x = lst[-1]
         else:
             x = 256
         return lookup[x]
+
     return f
 
 
 def weighted_positions(distance_weight, alpha, beta):
     def f(lst):
-        out = [0]*256
+        out = [0] * 256
         for i in reversed(range(max_distance)):
             if i < len(lst):
-                out = [distance_weight(i)*x + y for x, y in zip(positions[i][lst[-i-1]], out)]
+                out = [
+                    distance_weight(i) * x + y
+                    for x, y in zip(positions[i][lst[-i - 1]], out)
+                ]
             elif i == 0:
-                out = [distance_weight(i)*x + y for x, y in zip(positions[i][256], out)]
-        out = [x*(sum(frequencies)+256*alpha) + beta*(y + alpha) for x, y in zip(out, frequencies)]
+                out = [
+                    distance_weight(i) * x + y
+                    for x, y in zip(positions[i][256], out)
+                ]
+        out = [
+            x * (sum(frequencies) + 256 * alpha) + beta * (y + alpha)
+            for x, y in zip(out, frequencies)
+        ]
         return out
+
     return f
